708e6abf409d32930bc652743f18fc00440251e4
Katharyn Lynn Oggenfuss <koggenfuss@gmail.com>
2021-01-27 12:09:48 -0500

    3 dog pictures now appear

--- dogs.js
+++ dogs.js
@@ -5,11 +5,12 @@ xhr.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
         var img = JSON.parse(xhr.responseText);
         console.log(img);
-        document.getElementById('dog').src=img.message;
-        // document.getElementById('dog2').src=img.message;
-        // document.getElementById('dog3').src=img.message;
+
+        document.getElementById('dog1').src=img.message[0];
+        document.getElementById('dog2').src=img.message[1];
+        document.getElementById('dog3').src=img.message[2];
     }
 };
-xhr.open("GET", 'https://dog.ceo/api/breeds/image/random');
+xhr.open("GET", 'https://dog.ceo/api/breeds/image/random/3');
 xhr.send();
 }
