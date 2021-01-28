dc34f3dae2eb6222251cf0e5b982477f410199a0
Earl M. Smith <unixsa@gmail.com>
2021-01-28 18:03:35 -0500

    Thr33 Dogz

--- js/doggy.js
+++ js/doggy.js
@@ -1,12 +1,14 @@
 function dogs() {
-let xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200); {
-        var data = JSON.parse(this.responseText)
-    document.getElementById("dogs").src = data.message;
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200); {
+            let data = JSON.parse(this.responseText);
+        document.getElementById("dogs0").src = data.message[0];
+        document.getElementById("dogs1").src = data.message[1];
+        document.getElementById("dogs2").src = data.message[2];
+        }
+    };
+    
+    xhr.open("GET", "https://dog.ceo/api/breeds/image/random/3");
+    xhr.send();
     }
-};
-
-xhr.open("GET", "https://dog.ceo/api/breeds/image/random", true);
-xhr.send();
-}
