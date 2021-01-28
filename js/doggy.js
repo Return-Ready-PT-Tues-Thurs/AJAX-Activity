0ae7a1df1b0b4b95743ebde2eb97467af96e87d4
Earl M. Smith <unixsa@gmail.com>
2021-01-28 18:27:43 -0500

    fixed json error

--- js/doggy.js
+++ js/doggy.js
@@ -1,7 +1,7 @@
 function dogs() {
     let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200); {
+        if (this.readyState == 4 && this.status == 200) {
             let data = JSON.parse(this.responseText);
         document.getElementById("dogs0").src = data.message[0];
         document.getElementById("dogs1").src = data.message[1];
