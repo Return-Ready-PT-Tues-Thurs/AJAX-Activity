de5369e570fd34af4670c729fd81ceaed1bdef04
Kayanr <kayan.reid@gmail.com>
2021-01-28 17:22:10 -0500

    update to display random image
    update to display random image

--- happiness.js
+++ happiness.js
@@ -3,10 +3,10 @@ var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
   if (xhr.readyState === 4) {
     var n = JSON.parse(xhr.responseText);
-    var sec = JSON.parse(xhr.responseText);
+    //var sec = JSON.parse(xhr.responseText);
     //console.log(n.message);
     document.getElementById("dogImage").src = n.message[0];
-    document.getElementById("secImage").src = sec.message[1];
+    document.getElementById("secImage").src = n.message[1];
     document.getElementById("thirdImage").src = n.message[2];
   }
 };
