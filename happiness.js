e22c90b99a208ebdf6d05b848c58a9b0b5c39f43
Kayanr <kayan.reid@gmail.com>
2021-01-28 15:15:11 -0500

    add 3 images
    add 3 images

--- happiness.js
+++ happiness.js
@@ -5,15 +5,16 @@ xhr.onreadystatechange = function () {
     var n = JSON.parse(xhr.responseText);
     var sec = JSON.parse(xhr.responseText);
     //console.log(n.message);
-    document.getElementById("dogImage").src = n.message;
-    document.getElementById("secImage").src = sec.message;
-    document.getElementById("thirdImage").src = n.message;
+    document.getElementById("dogImage").src = n.message[0];
+    document.getElementById("secImage").src = sec.message[1];
+    document.getElementById("thirdImage").src = n.message[2];
   }
 };
 
-xhr.open("GET", "https://dog.ceo/api/breeds/image/random");
+xhr.open("GET", "https://dog.ceo/api/breeds/image/random/3");
+//https://dog.ceo/api/breeds/image/random/3
+//xhr.open("GET", "https://dog.ceo/api/breeds/image/random");
 
 function sendAJAX() {
   xhr.send();
 }
-
