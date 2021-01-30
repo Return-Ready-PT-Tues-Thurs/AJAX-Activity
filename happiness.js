8506049fac37e9f0ce97876be4a488b521f3762d
Kayanr <kayan.reid@gmail.com>
2021-01-30 17:07:14 -0500

    Add separate buttons and onclick AJAX functions
    Add separate buttons and onclick AJAX functions to display 1 dog and 3 random dogs.

--- happiness.js
+++ happiness.js
@@ -3,18 +3,31 @@ var xhr = new XMLHttpRequest();
 function sendAJAX() {
   xhr.onreadystatechange = function () {
     if (xhr.readyState === 4) {
-      var n = JSON.parse(xhr.responseText);
-      //var sec = JSON.parse(xhr.responseText);
-      //console.log(n.message);
-      document.getElementById("dogImage").src = n.message[0];
-      document.getElementById("secImage").src = n.message[1];
-      document.getElementById("thirdImage").src = n.message[2];
+      var singleDog = JSON.parse(xhr.responseText);
+      document.getElementById("singleImage").src = singleDog.message;
+      console.log(xhr.responseText);
+    }
+  };
+
+  xhr.open("GET", "https://dog.ceo/api/breeds/image/random");
+
+  xhr.send();
+}
+
+function sendAJAX3() {
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      var threeDogs = JSON.parse(xhr.responseText);
+
+      document.getElementById("dogImage").src = threeDogs.message[0];
+      document.getElementById("secImage").src = threeDogs.message[1];
+      document.getElementById("thirdImage").src = threeDogs.message[2];
+
+      console.log(xhr.responseText);
     }
   };
 
   xhr.open("GET", "https://dog.ceo/api/breeds/image/random/3");
-  //https://dog.ceo/api/breeds/image/random/3
-  //xhr.open("GET", "https://dog.ceo/api/breeds/image/random");
 
   xhr.send();
 }
