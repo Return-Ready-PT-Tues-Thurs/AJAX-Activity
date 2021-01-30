c870e0e43a42b165eacb418258895c7d657071cc
Kayanr <kayan.reid@gmail.com>
2021-01-30 15:41:43 -0500

    Move sendAJAX() function definition to start at line 3
    Move sendAJAX() function definition to start at line 3

--- happiness.js
+++ happiness.js
@@ -1,20 +1,20 @@
 var xhr = new XMLHttpRequest();
 
-xhr.onreadystatechange = function () {
-  if (xhr.readyState === 4) {
-    var n = JSON.parse(xhr.responseText);
-    //var sec = JSON.parse(xhr.responseText);
-    //console.log(n.message);
-    document.getElementById("dogImage").src = n.message[0];
-    document.getElementById("secImage").src = n.message[1];
-    document.getElementById("thirdImage").src = n.message[2];
-  }
-};
-
-xhr.open("GET", "https://dog.ceo/api/breeds/image/random/3");
-//https://dog.ceo/api/breeds/image/random/3
-//xhr.open("GET", "https://dog.ceo/api/breeds/image/random");
-
 function sendAJAX() {
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      var n = JSON.parse(xhr.responseText);
+      //var sec = JSON.parse(xhr.responseText);
+      //console.log(n.message);
+      document.getElementById("dogImage").src = n.message[0];
+      document.getElementById("secImage").src = n.message[1];
+      document.getElementById("thirdImage").src = n.message[2];
+    }
+  };
+
+  xhr.open("GET", "https://dog.ceo/api/breeds/image/random/3");
+  //https://dog.ceo/api/breeds/image/random/3
+  //xhr.open("GET", "https://dog.ceo/api/breeds/image/random");
+
   xhr.send();
 }
